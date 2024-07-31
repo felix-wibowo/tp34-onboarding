347b064e0061d5776edabf4544e79b55b80a4af6
Felix <felix.wibowo@monash.edu>
2024-08-01 01:56:32 +1000

    Resolve

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import herobackground from '/public/herobackground.jpg'
 
 const Hero = () =>  {
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import logo from "/public/logo-removebg.png";
 
 const Navbar = () => {
