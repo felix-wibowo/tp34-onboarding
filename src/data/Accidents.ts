3d0bcf6910b1877e56f0ad28223bafc4ed0de5f9
Felix <felix.wibowo@monash.edu>
2024-07-31 16:11:12 +1000

    Add logs

--- src/data/Accidents.ts
+++ src/data/Accidents.ts
@@ -2,13 +2,15 @@ import { logger } from "@/libs/Logger";
 import { dataClient } from "@/utils/AmplifyData";
 
 export const fetchAccidents = async () => {
-  const { data, nextToken } = await dataClient.models.accidents.list({
+  const { data, nextToken, errors } = await dataClient.models.accidents.list({
     limit: 10,
   });
 
   logger.info(
     `Fetch accidents successful. Next token present: ${nextToken ? "yes" : "no"}`,
   );
+
+  logger.info(errors);
 
   return data;
 };
