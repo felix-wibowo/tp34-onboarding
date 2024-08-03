d53e1850701b077b6f2d1f166a1fe10e40af21d2
Felix <felix.wibowo@monash.edu>
2024-08-03 17:16:48 +1000

    Add auth

--- src/data/BikeRoutes.ts
+++ src/data/BikeRoutes.ts
@@ -1,9 +1,14 @@
+import { logger } from "@/libs/Logger";
 import { dataClient } from "@/utils/AmplifyData";
 
 export const fetchBikeRoutes = async () => {
   const { data } = await dataClient.models.bike_routes.list({
     limit: 500,
   });
+
+  logger.info("Data = ", await dataClient.models.bike_routes.list({
+    limit: 500,
+  }));
 
   const features = data.map((d) => {
     return {
