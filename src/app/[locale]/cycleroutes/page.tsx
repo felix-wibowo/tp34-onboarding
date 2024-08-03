f0ce454697658a0f784b5a73c3fc17a188f2c10b
Felix <felix.wibowo@monash.edu>
2024-08-03 18:23:51 +1000

    test

--- src/app/[locale]/cycleroutes/page.tsx
+++ src/app/[locale]/cycleroutes/page.tsx
@@ -1,5 +1,9 @@
 import CycleRoutesComponent from "@/components/pages/CycleRoutesComponent";
 import { fetchBikeRoutes } from "@/data/BikeRoutes";
+import { Amplify } from "aws-amplify";
+
+import outputs from "amplify_outputs.json";
+Amplify.configure(outputs, { ssr: true });
 
 export default async function Page() {
   const bikeRoutes = await fetchBikeRoutes();
--- src/data/BikeRoutes.ts
+++ src/data/BikeRoutes.ts
@@ -1,25 +1,34 @@
+import { db } from "@/libs/Db";
 import { logger } from "@/libs/Logger";
-import { dataClient } from "@/utils/AmplifyData";
+import { bikeRouteSchema } from "@/models/Schema";
 
 export const fetchBikeRoutes = async () => {
-  const { data } = await dataClient.models.bike_routes.list({
-    limit: 500,
-  });
-
-  logger.info("Data = ", await dataClient.models.bike_routes.list({
-    limit: 500,
-  }));
-
-  const features = data.map((d) => {
-    return {
-      geometry: typeof d.geo_shape === "string" ? JSON.parse(d.geo_shape) : {},
-      properties: {}
-    }
-  })
+  const data = db.select().from(bikeRouteSchema);
+  logger.info(data);
   
+  // const { data } = await dataClient.models.bike_routes.list({
+  //   limit: 500,
+  // });
+
+  // logger.info("Data = ", await dataClient.models.bike_routes.list({
+  //   limit: 500,
+  // }));
+
+  // const features = data.map((d) => {
+  //   return {
+  //     geometry: typeof d.geo_shape === "string" ? JSON.parse(d.geo_shape) : {},
+  //     properties: {}
+  //   }
+  // })
+  
+  // const geoJSON = {
+  //   type: "FeatureCollection",
+  //   features: features
+  // }
+
   const geoJSON = {
     type: "FeatureCollection",
-    features: features
+    features: []
   }
 
   return geoJSON;
