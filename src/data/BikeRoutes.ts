863840b75617d010bb840252c14ff1c816c67b32
Felix <felix.wibowo@monash.edu>
2024-08-03 20:43:28 +1000

    Add geojson

--- src/data/BikeRoutes.ts
+++ src/data/BikeRoutes.ts
@@ -1,34 +1,19 @@
 import { db } from "@/libs/Db";
-import { logger } from "@/libs/Logger";
 import { bikeRouteSchema } from "@/models/Schema";
 
 export const fetchBikeRoutes = async () => {
-  const data = db.select().from(bikeRouteSchema);
-  logger.info(data);
+  const data = await db.select().from(bikeRouteSchema);
   
-  // const { data } = await dataClient.models.bike_routes.list({
-  //   limit: 500,
-  // });
-
-  // logger.info("Data = ", await dataClient.models.bike_routes.list({
-  //   limit: 500,
-  // }));
-
-  // const features = data.map((d) => {
-  //   return {
-  //     geometry: typeof d.geo_shape === "string" ? JSON.parse(d.geo_shape) : {},
-  //     properties: {}
-  //   }
-  // })
+  const features = data.map((d) => {
+    return {
+      geometry: d.geo_shape,
+      properties: {}
+    }
+  })
   
-  // const geoJSON = {
-  //   type: "FeatureCollection",
-  //   features: features
-  // }
-
   const geoJSON = {
     type: "FeatureCollection",
-    features: []
+    features: features
   }
 
   return geoJSON;
